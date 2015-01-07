7c921a96c6c293266618e970a12e01055e469fad
Steve Corona <thestevecorona@gmail.com>
2015-01-07 08:20:00 -0800

    beginnings of test suite

--- tests/SessionHandlerCookie.php
+++ tests/SessionHandlerCookie.php
@@ -3,5 +3,10 @@
 require 'vendor/autoload.php';
 
 class SessionHandlerCookieTest extends PHPUnit_Framework_TestCase {
+
+	function testCanConstruct() {
+		$sh = new SessionHandler\Cookie;
+		$this->assertInstanceOf('SessionHandler\Cookie', $sh);
+	}
 	
 }
