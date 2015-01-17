50f917fb63335ea201f2735f096188084122976a
Steve Corona <thestevecorona@gmail.com>
2015-01-17 14:43:54 -0800

    clean up indentation

--- src/SessionHandler/Storage/SecureCookie.php
+++ src/SessionHandler/Storage/SecureCookie.php
@@ -4,29 +4,29 @@ namespace SessionHandler\Storage;
 
 class SecureCookie {
 
-	private $hash_len;
-	private $hash_algo;
-	private $hash_secret;
+  private $hash_len;
+  private $hash_algo;
+  private $hash_secret;
 
-	public function __construct($hash_secret=null, $hash_len=128, $hash_algo="sha512") {
-		$this->hash_len  = $hash_len;
-    	$this->hash_algo = $hash_algo;
+  public function __construct($hash_secret=null, $hash_len=128, $hash_algo="sha512") {
+    $this->hash_len  = $hash_len;
+      $this->hash_algo = $hash_algo;
 
-    	// If hash secret is empty, we need to set a default one
-    	if (empty($hash_secret)) {
-      	$hash_secret = $this->default_hash_secret();
-    	}
+      // If hash secret is empty, we need to set a default one
+      if (empty($hash_secret)) {
+        $hash_secret = $this->default_hash_secret();
+      }
 
-    	$this->hash_secret = $hash_secret;
-	}
+      $this->hash_secret = $hash_secret;
+  }
 
-	public function has($key) {
+  public function has($key) {
 
-	}
+  }
 
-	public function get($name, $default=null) {
+  public function get($name, $default=null) {
 
-	$raw = base64_decode($this->storage->get($session_id));
+  $raw = base64_decode($this->storage->get($session_id));
     if (strlen($raw) < $this->hash_len) return '';
 
     // The cookie data contains the actual data w/ the hash concatonated to the end,
@@ -42,31 +42,31 @@ class SecureCookie {
     // If we calculate a different hash, we can't trust the data. Return an empty string.
     if ($hash_calculated !== $hash) return '';
 
-	}
+  }
 
-	public function make($name, $value, $minutes=0, $path=null, $domain=null, $secure=false, $httponly=true) {
+  public function make($name, $value, $minutes=0, $path=null, $domain=null, $secure=false, $httponly=true) {
     // Calculate a hash for the data and append it to the end of the data string
     $hash = hash_hmac($this->hash_algo, $data, $this->hash_secret);
     $data .= $hash;
 
     // Set a cookie with the data
     setcookie($session_id, base64_encode($data), time()+3600);
-	}
+  }
 
-	public function forget($name) {
-	setcookie($session_id, '', time());
-	}
+  public function forget($name) {
+  setcookie($session_id, '', time());
+  }
 
-	/**
-	* Calculates the default_hash_secret to use as a fallback if no secret is passed. It's
-	* a bad idea to rely on this in production.
-	*/
-	protected function default_hash_secret() {
-	// This is not good, it's easily leakable to the outside world,
-	// but it's predictable and doesn't require much server state. It's a bad
-	// idea to depend on this and probably won't work with multiple servers or
-	// with multiple PHP-FPM/Apache processes.
-	return md5(php_uname() . getmypid());
-	}
+  /**
+  * Calculates the default_hash_secret to use as a fallback if no secret is passed. It's
+  * a bad idea to rely on this in production.
+  */
+  protected function default_hash_secret() {
+  // This is not good, it's easily leakable to the outside world,
+  // but it's predictable and doesn't require much server state. It's a bad
+  // idea to depend on this and probably won't work with multiple servers or
+  // with multiple PHP-FPM/Apache processes.
+  return md5(php_uname() . getmypid());
+  }
 
 }
--- tests/SessionHandlerCookie.php
+++ tests/SessionHandlerCookie.php
@@ -4,32 +4,32 @@ require '../vendor/autoload.php';
 
 class SessionHandlerCookieTest extends PHPUnit_Framework_TestCase {
 
-	public function testCanConstruct() {
-		$sh = new SessionHandler\Cookie;
-		$this->assertInstanceOf('SessionHandler\Cookie', $sh);
-	}
+  public function testCanConstruct() {
+    $sh = new SessionHandler\Cookie;
+    $this->assertInstanceOf('SessionHandler\Cookie', $sh);
+  }
 
-	public function testWritesHashedCookie() {
-		$sh = new SessionHandler\Cookie;
-		session_set_save_handler($sh, false);
-		session_start();
+  public function testWritesHashedCookie() {
+    $sh = new SessionHandler\Cookie;
+    session_set_save_handler($sh, false);
+    session_start();
 
-		$_SESSION["foobar"] = "bar";
+    $_SESSION["foobar"] = "bar";
 
-		$this->assertEquals($_COOKIE[session_id()], "");
+    $this->assertEquals($_COOKIE[session_id()], "");
 
-		ob_end_clean();
-	}
+    ob_end_clean();
+  }
 
-	public function testOpen() {}
+  public function testOpen() {}
 
-	public function testRead() {}
+  public function testRead() {}
 
-	public function testWrite() {}
+  public function testWrite() {}
 
-	public function testDestroy() {}
+  public function testDestroy() {}
 
-	public function testGc() {}
+  public function testGc() {}
 
-	public function testClose() {}
+  public function testClose() {}
 }
